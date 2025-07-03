4f23ad285aad2598217b34d9e4f576800f406237
Adrianus-Bagas <bagasadrianus80@gmail.com>
2025-07-03 13:13:21 +0700

    php-dasar: tipe data

---
+++ TipeData.php
@@ -0,0 +1,21 @@
+<?php
+// tipe data char
+$tipeChar = 'a';
+
+// tipe data string
+$tipeString = "string";
+
+// tipe data integer
+$tipeInteger = 10;
+
+// tipe data float
+$tipeFloat = 10.9;
+
+// tipe data boolean
+$tipeBoolean = false;
+
+// konversi string ke integer
+$bilanganString = "10";
+$bilanganInteger = (int) $bilanganString;
+
+echo $bilanganInteger * 2;
